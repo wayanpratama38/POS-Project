5770c0fe96124eefa64b999704761b20bcf272e3
wayanpratama38 <wayanpratama38@gmail.com>
2025-12-17 02:09:05 +0700

    refactor: :recycle: refactore error middleware to recognize zod error, and handle it to user

--- backend/src/middlewares/ErrorLogMiddleware.js
+++ backend/src/middlewares/ErrorLogMiddleware.js
@@ -1,10 +1,19 @@
-const errorLog = (err, req, res, next) => {
-	console.log(err.stack);
+import {z} from 'zod';
 
+const errorLog = (err, req, res, next) => {
+	// console.log(err.stack);
+	// console.log(err.issues[0].code);
 	const statusCode = err.statusCode || 500;
 	const message = err.message || 'Internal Server Error';
 
-	res.status(statusCode).json({
+	if (err instanceof z.ZodError) {
+		return res.status(400).json({
+			status: 'fail',
+			message: `Terjadi masalah ${err.issues[0].code}, pada ${err.issues[0].path} karena memerlukan tipe data ${err.issues[0].expected}`,
+		});
+	}
+
+	return res.status(statusCode).json({
 		status: 'fail',
 		message: message,
 	});
