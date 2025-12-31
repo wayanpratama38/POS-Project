fe335b64870059ee8813a7981551d2e40aa6be6b
wayanpratama38 <wayanpratama38@gmail.com>
2025-12-31 14:25:44 +0700

    refactor: :fire: remove vue, start using vite + react

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,22 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-    tailwindcss()
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+  plugins: [react()],
 })
